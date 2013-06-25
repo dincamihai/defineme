d60e9347e1e0ad55468b9dd9c325c66dcdc3cb5a
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2013-06-25 22:13:54 +0300

    load items from json

--- defineme.py
+++ defineme.py
@@ -1,9 +1,12 @@
+import flask
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    with open('static/jsons/links.json') as f:
+        data = flask.json.loads(f.read())
+        return render_template('index.html', **data)
 
 if __name__ == "__main__":
     app.run()
